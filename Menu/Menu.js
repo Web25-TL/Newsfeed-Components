da57bce6a88e3baa6e61cb8848f38b551f2991b4
harjoth <harjoth.khara@gmail.com>
2019-10-28 19:24:14 -0700

    my solution with stretch

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,35 +1,25 @@
-/* This is the data we will be using, study it but don't change anything, yet. */
+//article flip
+TweenMax.staggerFrom(
+  '.article',
+  1,
+  { rotation: 360, scale: 1, delay: 0.3 },
+  0.8
+);
+// Toggle the "menu--open" class on your menu refence.
+const toggleMenu = () => {
+  menu.classList.toggle('menu--open');
+  TweenMax.to('.menu', 0.5, { width: '190px', opacity: '1' }),
+    TweenMax.to('.menu ul li', 0.5, { right: 30, delay: 1.5 });
+};
 
-let menuItems = [
-  'Students',
-  'Faculty',
-  "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
-];
+// Start Here: Create a reference to the ".menu" class
+const menu = document.querySelector('.menu');
+// console.log(menu);
 
-/* 
+// create a reference to the ".menu-button" class
+const menuButton = document.querySelector('.menu-button');
+// console.log(menuButton);
 
-  Step 1: Write a function that will create a menu component as seen below:
-
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
-
-  The function takes an array as its only argument.
-
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
-
-  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
-  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
-  Step 5: return the menu component.
-
-  Step 6: add the menu component to the DOM.
-  
-*/
+// Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
+// menuButton.addEventListener('click',() => toggleMenu());
